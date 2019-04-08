cc47b1d73c4e01210436198a97cb2c619eb8c337
Kibanos <37525207+Kibanos@users.noreply.github.com>
2019-04-08 22:49:32 +1000

    Update visionDetection.py

--- vision/visionDetection.py
+++ vision/visionDetection.py
@@ -7,10 +7,6 @@ __author__ = 'Jason_Love'
 #########################################
 
 
-
-################################################################################
-## imports
-################################################################################
 from imageai.Detection import VideoObjectDetection
 from imageai.Detection import ObjectDetection
 import os
@@ -18,110 +14,106 @@ import cv2
 import time
 
 
-###########################################################################################################
-## CLASS
-###########################################################################################################
-
 class visionDetection():
+
 
     ######
     ## Initialiser:
     ####
     ## Input:
-    ## execution_path = place where yolo file + image files are located
+    ## execution_path = place where yolo file is located
     ## modelFile = file name of model used
     ## i_or_v = 'i' for image, 'v' for video
     ####
     ## Output:
     ## text file named "detected_vid_listings_{day}_{month}_{year}"
     ######
-    def __init__(self,execution_path,modelFile="yolo.h5",i_or_v='i'):
+    def __init__(self, execution_path, i_or_v='i'):
 
         #log file
-        self.time=time.strftime("%d_%b_%Y",time.localtime(time.time()))
-        self.output=open("detected_listings_"+str(self.time)+".txt","w")
+        self.time = time.strftime("%d_%b_%Y", time.localtime(time.time()))
+        self.output = open("detected_listings_" + str(self.time) + ".txt", "w")
         self.output.close()
 
-        #work path
-        self.execution_path=execution_path
-
         #model
-        if i_or_v=='v':
+        if i_or_v == 'v':
             self.detector = VideoObjectDetection()
         else:
             self.detector = ObjectDetection()
 
         self.detector.setModelTypeAsYOLOv3()
-        self.detector.setModelPath(os.path.join(self.execution_path, modelFile))
+        self.detector.setModelPath(os.path.join(self.execution_path, "yolo.h5"))
         self.detector.loadModel()
 
-
-
-
+        
     ######
     ## Detection of live (i.e. unstored) still images
     ####
     ## Input:
     ## frame = image
+    ## autoLog = boolean; if want to log everything detected this = True
+    ##          else if want to log only certain object detected this = False
+    ##          and frameDetectionLog(detections, objectCounts) can be
+    ##          called by controller seperately
+    ## minProb = threshold probability of accepting detection
     ####
     ## Output:
     ## outFrame = image of type array with detection markings
-    ## detections = array of dictionaries containing name,probability,position of detected objects
-    ## output_count = dictionary of number of unique objects)
+    ## detections = array of dictionaries containing name,probability,
+    ##                position of detected objects
+    ## outputCounts = dictionary of number of unique objects
     ####
-    ## detections also logged in detection file if object being searched (self.search) is found
+    ## detections also logged in detection file if autoLog==True
     ######
-    def imageDetect(self,frame,log=False, minProb=40):
+    def imageDetect(self, frame, autoLog = False, minProb = 40):
 
-        outFrame,detections = self.detector.detectObjectsFromImage(input_image=frame, input_type="array", output_type="array",minimum_percentage_probability = minProb)
+        outFrame, detections = self.detector.detectObjectsFromImage(
+                             input_image = frame, input_type = "array",
+                             output_type = "array",
+                             minimum_percentage_probability = minProb)
             
         objectCounts = {}
         for eachItem in detections:
             if eachItem["name"] in objectCounts:
-                objectCounts[eachItem["name"]] = objectCounts[eachItem["name"]] + 1
+                objectCounts[eachItem["name"]] =
+                                            objectCounts[eachItem["name"]] + 1
             else:
                 objectCounts[eachItem["name"]] = 1
 
 
-        if log:
-            self.frameDetectionLog(detections,objectCounts)
+        if autoLog:
+            self.frameDetectionLog(detections, objectCounts)
 
-        return outFrame,detections,objectCounts
+        return outFrame, detections, objectCounts
 
 
     ######
     ## Log of what was found in frame
     ####
     ## Input:
-    ## frame_number = id of frame if in stream
-    ## output_array = array of dictionaries containing name,probability,position of detected objects
-    ## output_count = dictionary of number of unique objects
+    ## frameNumber = id of frame if in stream
+    ## outputArray = array of dictionaries containing name,probability,
+    ##                  position of detected objects
+    ## outputCount = dictionary of number of unique objects
     ####
     ## Output:
     ## update detection log file with new detection log
     ######
-    def frameDetectionLog(self,frameNumber=0, outputArray, outputCount):
-        self.output=open("detected_listings_"+str(self.time)+".txt","a")
+    def frameDetectionLog(self, frameNumber = 0, outputArray, outputCount):
+        self.output = open("detected_listings_" + str(self.time) + ".txt", "a")
 
         self.output.write("\nTime: ")
-        self.output.write(time.strftime("%a, %d %b %Y %H:%M:%S",time.localtime()))
-        self.output.write("\nNumber of unique objects detected: "+str(outputCount))
+        self.output.write(time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime()))
+        self.output.write("\nNumber of unique objects detected: "
+                          + str(outputCount))
 
         for obj in outputArray:
-            self.output.write("\n")
-            self.output.write(str(obj['name']) + " : " + str(obj["percentage_probability"]) )
+            self.output.write("\n\t")
+            self.output.write(str(obj['name']) + " : "
+                              + str(obj["percentage_probability"]) )
         
         self.output.write("\n")
         self.output.close()
-
-
-    ######
-    ## Clear the log
-    ######
-    def logClear():
-        self.output=open("detected_listings_"+str(self.time)+".txt","w")
-        self.output.close()
-
 
 
     ######
@@ -132,19 +124,14 @@ class visionDetection():
     ####
     ## continuously outputs detections in detection log file
     ######
-    def videoDetect(self,camera):
-        video_path = self.detector.detectObjectsFromVideo(camera_input=camera, save_detected_video=False
-                                , frames_per_second=2, minimum_percentage_probability=minProb,
-                                per_frame_function = self.frameDetectionLog)#,frame_detection_interval=10)
+    def videoDetect(self, camera):
+        video_path = self.detector.detectObjectsFromVideo(camera_input=camera,
+                                save_detected_video = False, frames_per_second = 2,
+                                minimum_percentage_probability = minProb,
+                                per_frame_function = self.frameDetectionLog)
+                                #,frame_detection_interval=10)
 
-  
-
-
-#################################################################################
-## Class end
-################################################################################
-
-    
+        
 ####
 ## if running this code file directly
 ####
