687811b8f46427247ecc139e13440265d25ace86
MatthewC221 <matthewchen42@gmail.com>
2019-04-13 14:16:33 +1000

    subprocess working, aggregate our detections

--- vision/visionInput.py
+++ vision/visionInput.py
@@ -10,43 +10,58 @@ import numpy as np
 import argparse
 from visionDetection import visionDetection
 
-# I think there's an issue with setting up the transmissionSubprocess
-# I might want it to be in a class, that has an active / not active state
-
 cmdFile = "./printTest.py"
 # If subprocess is still running, kill and send new data
 def spawnTransmissionSubprocess(transmissionSubprocess, message):
     system = platform.system()
-    if transmissionSubprocess:
-        print ("Subprocess active = " + str(transmissionSubprocess.poll()))
-    if transmissionSubprocess is not None and transmissionSubprocess.poll() \
+    if transmissionSubprocess and transmissionSubprocess.poll() \
         is None:
         if system == 'Linux':
             os.killpg(os.getpgid(transmissionSubprocess.pid), 
                 signal.SIGTERM)
         elif system == 'Windows':
-            os.kill(transmissionSubprocess.pid, signal.CTRL_C_EVENT)
+            subprocess.call(['taskkill', '/F', '/T', '/PID', 
+                str(transmissionSubprocess.pid)])
         else:
             print ("Kill for [Darwin] Mac")
 
     if system == 'Linux':
-        transmissionSubprocess = subprocess.Popen(["python", "./printTest.py",
+        transmissionSubprocess = subprocess.Popen(["python", cmdFile,
             str(message)], shell=True, preexec_fn=os.setsid)
     elif system == 'Windows':
-        transmissionSubprocess = subprocess.Popen(["python", "./printTest.py",
+        transmissionSubprocess = subprocess.Popen(["python", cmdFile,
             str(message)], shell=True, 
             creationflags=subprocess.CREATE_NEW_PROCESS_GROUP)
     else:
         print ("Create subprocess for [Darwin] Mac")
+
+    return transmissionSubprocess
+
+"""
+This method will aggregate the new detection results into our current 
+detections. This is done because a new detection should not override what
+we've accumulated so far.
+
+Imagine: (Transmission every 60 seconds, detect every 10 seconds)
+A man walks by the camera at :05, and exits at :10
+In transmission 1 at :60, we want to send {'person': 1}
+"""
+def aggregateDetections(newDetection, currentDetections):
+    for objective, count in newDetection.items():
+        if objective in currentDetections:
+            currentDetections[objective] = max(currentDetections[objective], 
+                count)
+        else:
+            currentDetections[objective] = count
 
 def main():
     parser = argparse.ArgumentParser(description='Performs recognition on \
         potential objects.')
     parser.add_argument('--dest', default="results", 
         help='specifies destination of log files if log is on')
-    parser.add_argument('--freqDetect', default=1.0, type=float,
+    parser.add_argument('--detectDelay', default=1.0, type=float,
         help='recognises every n seconds')
-    parser.add_argument('--freqTransmit', default=20.0, type=float,
+    parser.add_argument('--transmitDelay', default=20.0, type=float,
         help='transmits every n seconds')
     parser.add_argument('--target', default='person',
         help='recognition target, --showTargets will display all targets')
@@ -76,42 +91,44 @@ def main():
         return
 
     cap = cv2.VideoCapture(args.camIndex)
-    freqDetect = args.freqDetect
-    freqTransmit = args.freqTransmit
+    detectDelay = args.detectDelay
+    transmitDelay = args.transmitDelay
     shouldLog = args.log
     target = args.target
     threshold = args.threshold * 100
     openCVRed = (0, 0, 255)
     boundingBoxes = []
     DetectionSystem = visionDetection()
-
-    lastDetectionTime = time.time()
+   
     detectionResult = {}
-    lastTransmissionTime = time.time()
+    # Subtract the delays to start immediately
+    lastDetectionTime = time.time() - detectDelay 
+    lastTransmissionTime = time.time() - transmitDelay
     transmissionSubprocess = None
 
     while True:
         ret, frame = cap.read()
         currentTime = time.time()
-        # Detection: shows detection on frame and saves result
-        if currentTime >= lastDetectionTime + freqDetect:
+        # Detection: shows detection on frame and aggregates result
+        if currentTime >= lastDetectionTime + detectDelay:
             lastDetectionTime = currentTime
             boundingBoxes.clear()
-            _, detections, detectionResult = \
+            _, detections, newDetectionResult = \
                 DetectionSystem.imageDetect(frame, autoLog=shouldLog, 
                     minProb=threshold)
             for detect in detections:
                 if detect['name'] == target:
                     boundingBoxes.append(detect['box_points'])
             print ("Recognition objective [{0}]: |detected| = {1}"
-                .format(target, detectionResult.get(target, 0)))
+                .format(target, newDetectionResult.get(target, 0)))
+            aggregateDetections(newDetectionResult, detectionResult)
 
         # Transmission: creates an updated subprocess to send data
-        if currentTime >= lastTransmissionTime + freqTransmit:
+        if currentTime >= lastTransmissionTime + transmitDelay:
             lastTransmissionTime = currentTime
             if len(detectionResult) == 0: continue
-            spawnTransmissionSubprocess(transmissionSubprocess, 
-                detectionResult)
+            transmissionSubprocess = spawnTransmissionSubprocess(
+                transmissionSubprocess, detectionResult)
 
         for corners in boundingBoxes:
             cv2.rectangle(frame, (corners[0], corners[1]), (corners[2], \
