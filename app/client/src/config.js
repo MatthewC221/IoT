b28332bbc69307a80e54224253d593c02741cb76
Kibanos <37525207+Kibanos@users.noreply.github.com>
2019-04-29 16:03:42 +1000

    email sms notif patch (#14)
    * Notification functions and user listing
    
    * Update config.js
    
    added email config info
    
    * to refresh new code from master
    
    * Refresh with master
    
    * ...
    
    * completed email/sms notif
    
    Can change personal preferences for notification in app/server/notification/userData.json
    (note as using trial twilio account, numbers being sent to need to be pre-linked wit account before they can be used)
    
    * modified notif boolean for email/sms
    
    changed how email/sms booleans were defined in userData.json
    
    * update server/package.json
    
    forgot to include in previous commit
    
    * update www to notify once message comes
    
    due to master clash, re-did this udate,
    
    * edited some nitpicks + removed constructor from notify
    
    if in need of constructor hen running then an empty one "constructor(){}" would do.
    
    * mergerd userData.json into config.js
    
    * changed userProfile -> customerData
    
    to avoid clash with web client usage
    
    * renamed notify.js file and made constructor
    
    so as to avoid using either
    static function label or
    class.prototype.functon calling style
    
    * refactor name change in use
    
    Should have been included in last commit, was somehow left out
    
    * merge issues playaround
    
    * update config
    
    * dotenv package update

--- app/client/src/config.js
+++ app/client/src/config.js
@@ -1,16 +1,103 @@
+const path = require('path');
+require('dotenv').config({path: path.resolve(__dirname,'../../server/.env')});
+
 const productConfig = {
     name: 'Agri-Vision'
-}
+};
 
 const ttnConfig = {
-    appId: 'comp6324_8_testing',
-    accessKey: 'ttn-account-v2.kwfGp5IlnhPua5xrHsF9PeIH6LyWyPMKXGOgP7oVZ88'
-}
+    appId: process.env.AGRI_TTN,
+    accessKey: process.env.AGRI_KEY
+};
 
 const twilioConfig = {
-    accountSid: 'ACd8d455064f253c4a2e493a29b3213f77',
-    authToken: '912dc7896ae121ca6a13c471f58f78f0'
-}
+    accountSid: process.env.AGRI_SID,
+    authToken: process.env.AGRI_TOKEN,
+    sendingNumber: process.env.AGRI_NUM
+};
+
+const emailConfig = {
+    service: 'Gmail',
+    user: process.env.AGRI_USER,
+    pass: process.env.AGRI_PASS,
+    sendingEmail: 'Agri-Vision <G8@AgriVision.com>'
+};
+
+
+const modelMessage = [
+    {name: 'human', subject: 'TRESPASSER!!!!'},
+    {name: 'animalCount', subject: 'Number of animals seen'},
+    {name: 'pestID', subject: 'FERAL SPOTTED!!!'},
+    {name: 'vehicleID', subject: 'Vehicle Spotted!'}
+];
+
+const customerData = [
+    {
+        name: "Hugo",
+        email: "Hugo@Agri-vision.com",
+        phone: "+852933313165",
+        notif: {
+            email: false,
+            phone: false
+        },
+        devices: [
+            {id: "testing_device", model: "human"},
+            {id: "contol", model: "pestID"}
+        ]
+    },
+    {
+        name: "Jace",
+        email: "kibaov@gmail.com",
+        phone: "+61468440474",
+        notif: {
+            email: true,
+            phone: false
+        },
+        devices: [
+            {id: "testing_device", model: "human"},
+            {id: "anima", model: "animalCount"}
+        ]
+    },
+    {
+        name: "Matt",
+        email: "Matt@Agri-vision.com",
+        phone: "+61424082585",
+        notif: {
+            email: false,
+            phone: false
+        },
+        devices: [
+            {id: "testing_device", model: "human"},
+            {id: "car", model: "vehicleID"}
+        ]
+    },
+    {
+        name: "Max",
+        email: "Max@Agri-vision.com",
+        phone: "+61402829421",
+        notif: {
+            email: false,
+            phone: false
+        },
+        devices: [
+            {id: "testing_device", model: "human"},
+            {id: "ranger", model: "human"}
+        ]
+    },
+    {
+        name: "Mich",
+        email: "Mich@Agri-vision.com",
+        phone: "+61420314485",
+        notif: {
+            email: false,
+            phone: false
+        },
+        devices: [
+            {id: "testing_device", model: "human"},
+            {id: "animals", model: "animalCount"}
+        ]
+    }
+];
 
 const menuItems = [
     {name: "home page", icon: "home_heart.png"},
@@ -114,8 +201,11 @@ module.exports = {
     productConfig: productConfig,
     ttnConfig: ttnConfig,
     twilioConfig: twilioConfig,
+    emailConfig: emailConfig,
     menuItems: menuItems,
     storeModules: storeModules,
     memberProfiles: memberProfiles,
-    userProfile: userProfile
+    userProfile: userProfile,
+    modelMessage: modelMessage,
+    customerData: customerData
 }
---
+++ app/server/notification/notification_dispatcher.js
@@ -0,0 +1,85 @@
+var config = require('../../client/src/config');
+const nodemailer = require('nodemailer');
+var twilio = require('twilio')(config.twilioConfig.accountSid, config.twilioConfig.authToken);
+
+class Notification_Dispatcher {
+
+    constructor(){
+        Notification_Dispatcher.sendMail = this.sendMail.bind(this);
+        Notification_Dispatcher.sendSMS = this.sendSMS.bind(this);
+        Notification_Dispatcher.notifyOnTrespasser = this.notifyOnTrespasser.bind(this)
+    }
+
+    sendSMS(customer, subject, message){
+        return twilio.api.messages
+            .create({
+                body: `${subject}\n\n${message}`,
+                to: customer,
+                from: config.twilioConfig.sendingNumber,
+            }).then(function(data) {
+                console.log('Customer notified');
+            }).catch(function(err) {
+                console.error('Could not notify customer');
+                console.error(err);
+            });
+    };
+
+    sendMail(customer, mailSubject, mailBody, mailBodyhtml){
+
+        let transporter = nodemailer.createTransport({
+            service: config.emailConfig.service,
+            auth: {
+                user: config.emailConfig.user, // generated ethereal user
+                pass: config.emailConfig.pass // generated ethereal password
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        },
+        {
+            from: config.emailConfig.sendingEmail
+        });
+
+        transporter.sendMail({
+                to: customer,
+                subject: mailSubject,
+                text: mailBody, //plain text
+                html: mailBodyhtml
+        },
+            (error,info)=>{
+
+                if (error){
+                    return console.log(error.message);
+                }
+
+                console.log("Message sent: %s", info.messageId);
+        });
+    };
+
+    notifyOnTrespasser(deviceId,message){
+        //search for device
+        return config.customerData.forEach(function(customer) {
+            customer.devices.forEach(function(device){
+
+                if (device.id === deviceId) {
+                    //get info of model run on device
+                    let model = config.modelMessage[config.modelMessage.findIndex(
+                        model => model.name === device.model)];
+
+                    if (customer.notif.phone) {
+                        Notification_Dispatcher.sendSMS(customer.phone, model.subject, message);
+                    }
+
+                    if (customer.notif.email) {
+                        let htmlMessage = `<h1>${model.subject}</h1><b>${message}</b>`;
+                        Notification_Dispatcher.sendMail(customer.email, model.subject, message, htmlMessage);
+                    }
+                }
+            });
+        });
+    };
+}
+
+module.exports = {
+    Notification_Dispatcher
+};
